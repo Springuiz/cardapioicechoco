eee2653dd69a9372b2c3a49cc5cdb52ceabb5548
Guilherme <guilhermecosta301408@gmail.com>
2026-06-01 02:25:53 -0300

    intro.js

--- intro.js
+++ intro.js
@@ -1,6 +1,18 @@
 const video = document.getElementById("intro");
 const skipBtn = document.getElementById("skipBtn");
 
+// garante que não quebra se ainda não carregou
+video.addEventListener("loadeddata", () => {
+    video.play().catch(() => {});
+});
+
+btn.addEventListener("click", () => {
+    video.pause();
+    video.currentTime = video.duration; // pula pro final
+    video.style.display = "none";
+    btn.style.display = "none";
+});
+
 function entrarNoSite() {
    video.style.opacity = "0";
 
@@ -12,4 +24,3 @@ function entrarNoSite() {
 video.addEventListener("ended", entrarNoSite);
 
 skipBtn.addEventListener("click", entrarNoSite);
-
